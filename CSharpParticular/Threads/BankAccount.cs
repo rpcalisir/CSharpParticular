6d22f95080800f2a2c70ed8f136b3f63ecb4c915
Recep Calisir <recep.calisir@dormakaba.com>
2020-10-26 11:14:40 +0100

    Thread project is added

---
+++ CSharpParticular/Threads/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    public class BankAccount
+    {
+        private Object acctLock;
+
+        public double Balance { get; set; }
+
+        public BankAccount(double balance)
+        {
+            acctLock = new Object();
+            Balance = balance;
+        }
+
+        public double Withdraw(double amount)
+        {
+            if ((Balance-amount)<0)
+            {
+                Console.WriteLine($"Sorry!!  ${Balance} in account");
+                return Balance;
+            }
+            lock(acctLock)
+            {
+                if (Balance >= amount)
+                {
+                    Console.WriteLine("Removed {0} and {1} left in Account", amount, (Balance-amount));
+                    Balance -= amount;
+                }
+                return Balance;
+            }
+        }
+
+        public void MockingWithdraw()
+        {
+            Withdraw(1);
+        }
+    }
+}
---
+++ CSharpParticular/Threads/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount(10);
+            Thread[] threads = new Thread[15];
+
+            Thread.CurrentThread.Name = "main";
+
+            //Initialize thread names and add into thread array. This creates threads and adds into array.
+            for (int i = 0; i < 15; i++)
+            {
+                Thread t = new Thread(new ThreadStart(bankAccount.MockingWithdraw));
+                t.Name = i.ToString();
+                threads[i] = t;
+            }
+
+            //Execute threads with starting them one by one.
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Thread {0} Alive: {1}", threads[i].Name, threads[i].IsAlive);
+                threads[i].Start();
+                Console.WriteLine("Thread {0} Alive: {1}", threads[i].Name, threads[i].IsAlive);
+            }
+
+            Console.WriteLine("Current priority: {0}", Thread.CurrentThread.Priority);
+
+            Console.WriteLine("Thread {0} ending", Thread.CurrentThread.Name);
+
+            Console.ReadLine();
+        }
+    }
+}
